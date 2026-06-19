72974bccafee2726e5377362c33d0dffedd4b350
Kauan Augusto Naves <kauannazanaves0@gmail.com>
2026-06-18 21:41:38 -0300

    Template inicial de adicionar itens criado
    Criado o template e formulário inicial para adicionar itens no sistema.
    Banco de Dados fictício usando lista de dicionários criado e populado.

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from database.db import ITENS
 
 app = Flask(__name__)
 
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["SECRET_KEY"] = "SUA-CHAVE-SECRETA-AQUI"
 
 # Rotas
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("pages/index.html")
+
+@app.route("/adicionar-item", methods=['GET', 'POST'])
+def adicionarItens():
+    if request.method == "GET":
+        return render_template("pages/adicionarItem.html")
+
+    return redirect("/")
